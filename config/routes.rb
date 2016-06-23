65a26b57de9994ca97317147443e50d54c0ddf42
Forrest Stone <iloveburningman@gmail.com>
2016-06-23 16:47:44 -0700

    Section 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+
+  devise_for :users
+
   get 'welcome/home'
-
   get 'welcome/about'
-
   get 'welcome/contact'
 
   root 'welcome#home'
