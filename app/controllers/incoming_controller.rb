3a08b5bd2e6a810bc9321c7bebba5b61cae85beb
Forrest Stone <iloveburningman@gmail.com>
2016-07-06 18:54:28 -0700

    chapter 6 done, also I believe I finished chapter 7 with on small if block in the views/topic/show page

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,17 +3,13 @@ class IncomingController < ApplicationController
  skip_before_action :verify_authenticity_token, :authenticate_user!, only: [:create]
 
  def create
-  #binding.pry
    @user = User.find_by(email: params[:sender])
    @topic = @user.topics.find_or_create_by(title: params[:Subject])
    @bookmark = @topic.bookmarks.build( url: params["body-plain"])
    @bookmark.save
-   # Step 1: Look up User based on sender.
-   # Step 2: Look up Topic based on subject.
-   # Step 3: Create bookmark that's associated with Topic and User based on body.
-   puts @user
-   puts @topic
-   puts @bookmark
+   puts "#{@user.id}"
+   puts  "#{@topic}"
+   puts  "#{@bookmark}"
    ap @user
    ap @topic
    ap @bookmark
