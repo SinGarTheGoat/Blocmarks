0d12729f270c5e75f8246780088193c7dafaa87d
Forrest Stone <iloveburningman@gmail.com>
2016-07-11 15:13:37 -0700

    minor fromats

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  before_action :authenticate_user!
-  include Pundit
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    before_action :authenticate_user!
+    include Pundit
+    protect_from_forgery with: :exception
 
- rescue_from Pundit::NotAuthorizedError , with: :user_not_authorized
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
- private
+    private
 
- def user_not_authorized
-   flash[:alert] = "You are not authorized to peform that action"
-   redirect_to(request.referrer || root_path)
- end
+    def user_not_authorized
+        flash[:alert] = 'You are not authorized to peform that action'
+        redirect_to(request.referrer || root_path)
+    end
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  it "vallidates url is present" do
-    expect(Bookmark.new()).to_not be_valid
-
-  end
-  it "validates http is set" do
-    expect(Bookmark.new(url: "www.google.com")).to be_valid
-  end
-
-
+    it 'vallidates url is present' do
+        expect(Bookmark.new).to_not be_valid
+    end
+    
+    it 'validates http is set' do
+        expect(Bookmark.new(url: 'www.google.com')).to be_valid
+    end
 end
