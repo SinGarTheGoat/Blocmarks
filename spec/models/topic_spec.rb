65191e6c2573bb83bff4bfd6adc39a039bab9e12
Forrest Stone <iloveburningman@gmail.com>
2016-07-14 14:13:00 -0700

    model/topic spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:user) { create(:user) }
+    let(:topic) { Topic.create(title: 'Hey this is a great title', user: user)}
+    it { is_expected.to belong_to(:user) }
+
+    describe 'attributes' do
+        it 'has a title' do
+            expect(topic).to have_attributes(title: 'Hey this is a great title')
+        end
+    end
 end
