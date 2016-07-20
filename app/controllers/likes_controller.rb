f7aef634f13edf23b96511e173a7028bd1713f28
Forrest Stone <iloveburningman@gmail.com>
2016-07-20 16:37:04 -0700

    get redirection right

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,20 +6,24 @@ class LikesController < ApplicationController
    like = current_user.likes.build(bookmark: @bookmark)
 
    if like.save
-     # Add code to generate a success flash and redirect to @bookmark
+       flash[:notice] = "Successfully liked bookmark."
    else
-     # Add code to generate a failure flash and redirect to @bookmark
+     flash.now[:alert] = 'Error in liking bookmark. Please try again.'
    end
+     redirect_to topics_path#(params[:id])
  end
 
  def destroy
+   @bookmark = Bookmark.find(params[:bookmark_id])
+   like = current_user.likes.find(params[:id])
    # Get the bookmark from the params
    # Find the current user's like with the ID in the params
 
    if like.destroy
-     # Flash success and redirect to @bookmark
+     flash[:notice] = "Successfully unliked bookmark."
    else
-     # Flash error and redirect to @bookmark
+     flash.now[:alert] = 'Error in unliking bookmark. Please try again.'
    end
+   redirect_to topics_path#(params[:id])
  end
 end
